3ca68aaeceb18ae7a37cef7810f7f5abd5482d80
Nermin <nermin93@gmx.at>
2020-03-30 23:59:30 +0200

    initial init

---
+++ UE_01/src/test/java/at/jku/swtesting/RingBufferTest.java
@@ -0,0 +1,13 @@
+package at.jku.swtesting;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RingBufferTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");  //TODO: Implement tests
+	}
+
+}
